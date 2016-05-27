ba0979c7ab9547e79b8559490970870a10251e8d
Alex T <alextsai0227@gmail.com>
2016-05-27 13:53:36 +0800

    Initial Commit

---
+++ alphaCamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alphaCamp
+//
+//  Created by 蔡舜珵 on 2016/5/27.
+//  Copyright © 2016年 蔡舜珵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
